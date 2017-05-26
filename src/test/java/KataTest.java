6ad9e7fbd68ded86ab40a9e9881383f9687108a9
alfredo.gil <alfredo.gil@oramon.nl>
2017-05-26 11:44:51 +0200

    Initial_Data

--- src/test/java/KataTest.java
+++ src/test/java/KataTest.java
@@ -1,6 +1,5 @@
-import com.fizzbuzz.impl.FizzBuzzImpl;
+
 import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
